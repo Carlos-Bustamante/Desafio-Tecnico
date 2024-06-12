84efaa649c754fa59cb92661ce51ec75f632251e
Carlos Montaño <cmontanobustamante@frba.utn.edu.ar>
2024-06-12 15:48:50 -0300

    Update .csproj

---
+++ Models/Entities/Producto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio_Tecnico.Models.Entities
+{
+    internal class Producto
+    {
+        [Key]
+        [Column("ID")]
+        public int Id { get; set; }
+    }
+}
---
+++ Models/Entities/TipoProducto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio_Tecnico.Models.Entities
+{
+    internal class TipoProducto
+    {
+        [Key]
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Column("DESCRIPCION")]
+        public int Descripcion { get; set; }
+
+    }
+}
